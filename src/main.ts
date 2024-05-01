67e31a735b6db49905eb628fbabe8ae5b23cb655
Olivier Deschênes <olivierdeschenes9@gmail.com>
2024-04-30 23:20:47 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import { VueQueryPlugin } from "@tanstack/vue-query";
-// @ts-expect-error
 import App from "./App.vue";
 
 createApp(App).use(VueQueryPlugin).mount("#app");
