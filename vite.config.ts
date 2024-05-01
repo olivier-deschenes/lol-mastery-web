571e6d6d80cbe73c1c827a32f253986c3ad7d7e6
Olivier Deschênes <olivierdeschenes9@gmail.com>
2024-04-30 23:16:11 -0400

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ mode }: { mode: string }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  });
+};
